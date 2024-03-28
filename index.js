d0c367d0f499790b6e0db0bc0b151622fbc8c88f
Saqib Masood <masoodsaqib00@gmail>
2024-03-28 16:56:48 +0530

    Chat Bot made using websockets

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+const app = express();
+const server = createServer(app);
+
+
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/index.html');
+});
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+// });
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+    //   console.log('message: ' + msg);
+    io.emit('chat message', msg);
+});
+  });
+
+  
+
+server.listen(3000, () => {
+  console.log('server running at http://localhost:3000');
+});
